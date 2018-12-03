7e02c8ab289417dd5392733c115f482929d48a48
peteryoon11 <yjisub11@gmail.com>
2018-12-04 00:43:37 +0900

    다운 로드 모듈을 붙였다. 그런데 정확히 이미지의 주소만 가져오지 않는다..

--- test_Code/webCrawling/main.go
+++ test_Code/webCrawling/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -41,6 +44,7 @@ func improvGetWebSourceFunc() {
 	defer resp.Body.Close()
 
 	z := html.NewTokenizer(resp.Body)
+	i := 0
 	for {
 		tt := z.Next()
 
@@ -57,17 +61,23 @@ func improvGetWebSourceFunc() {
 				//fmt.Println("We found a link!")
 				fmt.Println(t.Data)
 			} */
+
 			for _, a := range t.Attr {
-				fmt.Println("key = ", a, " ")
-				if a.Key == "img" {
+
+				//	fmt.Println("key = ", a, " ")
+				//if a.Key == "img" {
+				if a.Key == "src" {
 					//fmt.Println("Found href:", a.Val)
-					if strings.Contains(a.Val, "http") {
+					if strings.Contains(a.Val, "cdn") {
 						fmt.Println(a.Val)
+						tempInt := strconv.Itoa(i)
+						i++
+						DownloadFile("./temp/"+tempInt+".jpg", a.Val)
 					}
-					break
+					//break
 				}
 			}
-			fmt.Println("=")
+			//fmt.Println("=")
 		}
 	}
 
@@ -77,4 +87,28 @@ func improvGetWebSourceFunc() {
 	str := string(bytes) //바이트를 문자열로
 	fmt.Println(str)
 	*/
+}
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
