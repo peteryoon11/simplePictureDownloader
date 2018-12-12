340f9730faf904bfa87e3ff8d2fb28a5248d7b7d
peteryoon11 <yjisub11@gmail.com>
2018-12-12 11:23:09 +0900

    add code for detail function test code about merge check time and record log  in testcode folder

--- test_Code/TestLogging/main.go
+++ test_Code/TestLogging/main.go
@@ -11,8 +11,17 @@ import (
 var myLogger *log.Logger
 
 func main() {
-	//LoggingCore()
-	TestTimePrint()
+	LoggingCore()
+	p := fmt.Println
+	now := time.Now()
+	p(now)
+	//time.Sleep(100 * time.Millisecond)
+	time.Sleep(2 * time.Second)
+
+	diff := now.Sub(time.Now())
+	p(diff)
+	p(diff * (-1))
+	//TestTimePrint()
 }
 func LoggingCore() {
 	// 로그파일 오픈
@@ -40,9 +49,10 @@ func LoggingCore() {
 	// 파일과 화면에 같이 출력하기 위해 MultiWriter 생성
 	multiWriter := io.MultiWriter(fpLog, os.Stdout)
 	log.SetOutput(multiWriter)
+	myLogger = log.New(fpLog, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 
 	run()
-	log.Println("End of Program")
+	myLogger.Println("End of Program")
 }
 func TestTimePrint() {
 	p := fmt.Println
@@ -52,6 +62,7 @@ func TestTimePrint() {
 
 	then := time.Date(
 		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	p("=======================")
 	p(then)
 
 	p(then.Year())
@@ -70,6 +81,7 @@ func TestTimePrint() {
 	p(then.Equal(now))
 
 	diff := now.Sub(then)
+	p("=======================")
 	p(diff)
 
 	p(diff.Hours())
